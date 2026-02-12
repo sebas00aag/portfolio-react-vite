f49bcc7b969d491aa376afd6c66f8a5978f15572
sarrietag <sarrietag@ucenfotec.ac.cr>
2026-02-12 14:58:53 -0600

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,36 +1,24 @@
 export const projects = [
-    {
-        id: "rockpaperscissors",
-        title: "Rock Paper Scissors Game",
-        image: "/projects/RPS.png",
-        description:
-            "A classic game with basic AI logic for the computer, animations, and persistent scoring using localStorage.",
-        tech: ["HTML", "CSS", "JavaScript", "Responsive UI"],
-        demoUrl: "https://sebas00aag.github.io/Rock-Paper-Scissors-Game/",
-        codeUrl: "https://github.com/sebas00aag/Rock-Paper-Scissors-Game",
 
+    {
+        id: "dreamincode",
+        title: "DreamInCode",
+        image: "/projects/DreamInCode.png",
+        description:
+            "Web platform developed for managing reminders with user authentication system and dynamic real-time notifications, structured under a scalable modular architecture.",
+        tech: ["React", "JavaScript", "ContextAPI", "ReactRouter", "REST API", "Polling for Dynamic Notifications"],
+        demoUrl: "https://web-dream-in-code.vercel.app/",
+        codeUrl: "https://github.com/DreamInCode-CR/Web_DreamInCode",
     },
     {
-        id: "weatherapi",
-        title: "Weather API",
-        image: "/projects/WeatherApp.png",
+        id: "museai",
+        title: "MuseAI",
+        image: "/projects/MuseAI.png",
         description:
-            "An app that fetches weather data via an API, displays the forecast, and allows users to search by city with an intuitive UI.",
-        tech: ["HTML", "CSS", "JavaScript", "OpenWeather API", "Responsive UI"],
-        demoUrl: "https://sebas00aag.github.io/WeatherApp/",
-        codeUrl: "https://github.com/sebas00aag/WeatherApp",
-
-    },
-    {
-        id: "tictactoe",
-        title: "Tic Tac Toe Game",
-        image: "/projects/TTT.png",
-        description:
-            "A Tic-Tac-Toe game with winner logic, cell animations, and options for two players or against an AI.",
-        tech: ["HTML", "CSS", "JavaScript", "Responsive UI"],
-        demoUrl: "https://sebas00aag.github.io/TicTacToeGame/",
-        codeUrl: "https://github.com/sebas00aag/TicTacToeGame",
-
+            "3D virtual museum built with Angular and Three.js featuring FPS navigation, raycasting interaction, guided tours, and real museum API integration.",
+        tech: ["Angular", "Three.js", "TypeScript", "WebGL", "GLTF Loader", "Raycasting", "REST API Integration", "Responsive UI"],
+        demoUrl: "https://github.com/sebas00aag/MuseAI-FrontEnd",
+        codeUrl: "https://github.com/sebas00aag/MuseAI-FrontEnd",
     },
     {
         id: "hotelbooking",
@@ -44,23 +32,37 @@ export const projects = [
 
     },
     {
-        id: "museai",
-        title: "MuseAI",
-        image: "/projects/MuseAI.png",
+        id: "weatherapi",
+        title: "Weather API",
+        image: "/projects/WeatherApp.png",
         description:
-            "3D virtual museum built with Angular and Three.js featuring FPS navigation, raycasting interaction, guided tours, and real museum API integration.",
-        tech: ["Angular", "Three.js", "TypeScript", "WebGL", "GLTF Loader", "Raycasting", "REST API Integration", "Responsive UI"],
-        demoUrl: "https://github.com/sebas00aag/MuseAI-FrontEnd",
-        codeUrl: "https://github.com/sebas00aag/MuseAI-FrontEnd",
+            "An app that fetches weather data via an API, displays the forecast, and allows users to search by city with an intuitive UI.",
+        tech: ["HTML", "CSS", "JavaScript", "OpenWeather API", "Responsive UI"],
+        demoUrl: "https://sebas00aag.github.io/WeatherApp/",
+        codeUrl: "https://github.com/sebas00aag/WeatherApp",
+
+    },
+
+    {
+        id: "tictactoe",
+        title: "Tic Tac Toe Game",
+        image: "/projects/TTT.png",
+        description:
+            "A Tic-Tac-Toe game with winner logic, cell animations, and options for two players or against an AI.",
+        tech: ["HTML", "CSS", "JavaScript", "Responsive UI"],
+        demoUrl: "https://sebas00aag.github.io/TicTacToeGame/",
+        codeUrl: "https://github.com/sebas00aag/TicTacToeGame",
+
     },
     {
-        id: "dreamincode",
-        title: "DreamInCode",
-        image: "/projects/DreamInCode.png",
+        id: "rockpaperscissors",
+        title: "Rock Paper Scissors Game",
+        image: "/projects/RPS.png",
         description:
-            "Web platform developed for managing reminders with user authentication system and dynamic real-time notifications, structured under a scalable modular architecture.",
-        tech: ["React", "JavaScript", "ContextAPI", "ReactRouter", "REST API", "Polling for Dynamic Notifications"],
-        demoUrl: "https://web-dream-in-code.vercel.app/",
-        codeUrl: "https://github.com/DreamInCode-CR/Web_DreamInCode",
-    },
+            "A classic game with basic AI logic for the computer, animations, and persistent scoring using localStorage.",
+        tech: ["HTML", "CSS", "JavaScript", "Responsive UI"],
+        demoUrl: "https://sebas00aag.github.io/Rock-Paper-Scissors-Game/",
+        codeUrl: "https://github.com/sebas00aag/Rock-Paper-Scissors-Game",
+
+    }
 ];
